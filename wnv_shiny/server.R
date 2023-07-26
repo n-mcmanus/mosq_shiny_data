5c73d363b4a8a8cb36250c968710e3d0f96fadef
n-mcmanus <nmcmanus@bren.ucsb.edu>
2023-07-25 17:42:37 -0700

    organize code and update leaflet map

--- wnv_shiny/server.R
+++ wnv_shiny/server.R
@@ -1,11 +1,4 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+### READ IN DATA AND PACKAGES
 
 library(shiny)
 library(tidyverse)
@@ -15,49 +8,66 @@ library(raster)
 library(sf)
 library(here)
 
+## Rasters
 wnv_risk <- raster(here('data/Kern_transmission_raster_wgs84.tif'))
-pal <- colorNumeric(c("#0C2C84", "#41B6C4", "#FFFFCC"), values(trans_r),
-                    na.color = "transparent")
-water_test_data <- read_csv(here('data/water/water_test_data.csv')) %>% 
-  mutate(date = mdy(date))
+water <- raster(here('data/water/water_reproj.tif'))
+
+## Vectors
 zips <- read_sf(here("data/zipcodes/kern_zips.shp"))
 zips_trans<- st_transform(zips, crs = "+proj=longlat +datum=WGS84")
 
-# Define server logic required to draw a histogram
+## Data frames
+water_test_data <- read_csv(here('data/water/water_test_data.csv')) %>% 
+  mutate(date = mdy(date))
+
+## Raster color palette
+pal <- colorNumeric(palette = 'viridis', domain = values(wnv_risk),
+                    reverse = TRUE,
+                    na.color = "transparent")
+
+
+
+
+### START SERVER
 function(input, output, session) {
 
   
-  ## Zipcode input
+  ### Zip code input -----------------------------------------
   
+  ## React to user input and slightly delay response
   zipcode <- reactive(input$zip_box)
   zipcode_d <- debounce(zipcode, millis = 2000)
   
+  ## Return error if user submits a number
+  ## with length other than 5 or 0 (e.g. no entry)
   observeEvent(zipcode_d(), {
-    if (nchar(zipcode_d()) != 5)
+    if (nchar(zipcode_d()) != 5 & nchar(zipcode_d()) != 0)
     {
-      updateTextInput(session, 'zip_box')
+      updateTextInput(session, 'zip_box', value = NA)
       showModal(modalDialog(
           title = "Error!",
           "Only 5-character entries are permitted.",
-          easyClose = TRUE
-        )
+          easyClose = TRUE)
       )
     }
   })
   
-  ## Filter water data based on zipcode
+  ## Filter water data based on zip input
   water_zip <- reactive({
-    
-    water_data_filtered <- water_test_data %>% 
+    data_filtered <- water_test_data %>% 
       filter(zipcode == zipcode_d()) 
     
-    return(water_data_filtered)
+    return(data_filtered)
   })           
+
   
-  ## Water plot
+  ### Panel plots --------------------------------------------
+  
+  ## Standing water time series
   output$water_plot <- renderPlot({
-   if (is.na(zipcode_d()))
+   if (is.na(zipcode()))
      return(NULL)
+     
     
     ggplot(data = water_zip(), aes(x = date, y = water_ha)) +
       geom_point(color = "dodgerblue3", size = 4, alpha = 0.6) +
@@ -74,49 +84,47 @@ function(input, output, session) {
         axis.title.y = element_text(face = 'bold', vjust = 3, size = 15),
         axis.text = element_text(size = 13)
       )
-        
-      
-    
+   
   })
   
   
   
-  ## Leaflet map
+  ### Leaflet map ---------------------------------------------
   output$map <- renderLeaflet({
     leaflet() %>% 
       ## Add background maps
-      addTiles(group = "OSM (default)") %>% 
+      addTiles(group = "OpenStreetMaps") %>% 
       addProviderTiles(providers$Stamen.TonerLite, group = "Toner Lite") %>%
       ## Add rasters
-      addRasterImage(wnv_risk, colors = pal, project = FALSE, group = "WNV Risk") %>%
+      addRasterImage(wnv_risk, colors = pal, 
+                     project = FALSE, group = "WNV Risk") %>%
+      # addRasterImage(water_reproj, colors = 'dodgerblue4', 
+      #                project = FALSE, group = "Standing Water") %>% 
       ## Add polygons
       addPolygons(data = zips_trans, color = "#343434", 
                   weight = 2, fillOpacity = 0.1,
                   label = paste0("Zip code: ", zips_trans$GEOID10),
+                  highlight = highlightOptions(weight = 5,
+                                               color = "white",
+                                               bringToFront = TRUE),
                   group = "Zip codes") %>% 
       ## Add groups to map
       addLayersControl(
-        baseGroups = c("OSM (default)", "Toner Lite"),
-        overlayGroups = c("WNV Risk", "Zip codes"),
-        options = layersControlOptions(collapsed = FALSE)
-      )
-    
+        baseGroups = c("OpenStreetMaps", "Toner Lite"),
+        overlayGroups = c("WNV Risk", "Zip codes", "Standing Water"),
+        options = layersControlOptions(collapsed = TRUE),
+        position = "topleft") %>% 
+      ## Only raster shows by default
+      hideGroup(c("Zip codes", "Standing Water")) %>% 
+      ## Add legend to map
+      addLegend(pal = pal, values = values(wnv_risk),
+                position = "bottomleft",
+                title = "WNV Transmission Risk") %>% 
+      setView(-119.3, 35.55, zoom = 10)
   })
   
   
   
-  
-    # output$distPlot <- renderPlot({
-    # 
-    #     # generate bins based on input$bins from ui.R
-    #     x    <- faithful[, 2]
-    #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    # 
-    #     # draw the histogram with the specified number of bins
-    #     hist(x, breaks = bins, col = 'darkgray', border = 'white',
-    #          xlab = 'Waiting time to next eruption (in mins)',
-    #          main = 'Histogram of waiting times')
-    # 
-    # })
 
-}
+
+} ### END SERVER
